941dea6380f9edaa43396229d068453074351802
Artem Ershov <colombadoro@ya.ru>
2023-09-18 23:09:40 +0300

    complite home page, contact page

--- catalog/views.py
+++ catalog/views.py
@@ -3,4 +3,12 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    pass render(request, )
+    return render(request, 'catalog/index.html')
+
+def contact(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+        print(name, email, message)
+    return render(request, 'catalog/contact.html')
