3b63fd3d7e2e42385a2f08decb13178d5a49bb2f
Artem Ershov <colombadoro@ya.ru>
2023-10-14 00:14:22 +0300

    in the process of creating Authentication

---
+++ users/models.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+from catalog.models import NULLABLE
+
+
+class User(AbstractUser):
+    username = None
+
+    email = models.EmailField(unique=True, verbose_name='email')
+    phone = models.CharField(max_length=255, verbose_name='номер телефона', **NULLABLE)
+    country = models.CharField(max_length=255, verbose_name='страна', **NULLABLE)
+    avatar = models.ImageField(upload_to='users/', verbose_name='аватар', **NULLABLE)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
